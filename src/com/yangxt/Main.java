6ae14dbced8ab933fd2356ac53a15ee7a68caaa4
Yangxiaotian <stdlib@foxmail.com>
2017-07-20 13:23:48 +0800

    1

--- src/com/yangxt/Main.java
+++ src/com/yangxt/Main.java
@@ -1,8 +1,9 @@
 package com.yangxt;
 
+import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		UI ui = new UI();
 		ui.createUI();
 	}
